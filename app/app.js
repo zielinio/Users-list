91b803ca727ce9d371d8b5e7c8121398306e9b6c
zielinio <wdtz.plzaq1>
2016-01-19 19:24:34 +0100

    add views: abstract, main layout, edit user modal and users table + controllers

--- app/app.js
+++ app/app.js
@@ -1,21 +1,43 @@
 var pragmatistsApp = angular.module('pragmatistsApp', ['ui.router']);
 
 pragmatistsApp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/list");
 
     $stateProvider
+        .state('app', {
+            abstract: true,
+            views: {
+                "": {
+                    templateUrl: "views/layout.html"
+                }
+            }
+        })
         .state('list', {
-            url: "/",
+            url: "/list",
+            parent: 'app',
             templateUrl: "views/list.html",
             controller:"UsersController",
             controllerAs:"vm",
             resolve: {
-                users: function(){
+                users: function() {
                     return [
                         { name: 'Neddard Stark', email: 'ned@stark.got'},
                         { name: 'Arya Stark', email: 'ned@stark.got'},
                         { name: 'Stannis Lannister', email: 'ned@stark.got'}
                     ];
+                }
+            }
+        })
+        .state('list.user', {
+            url: "/user/:index",
+            views: {
+                "modal@": {
+                    templateUrl: "views/modal-layout.html"
+                },
+                "@list.user": {
+                    templateUrl: "views/edit.html",
+                    controller: 'EditController',
+                    controllerAs:"vm",
                 }
             }
         });
@@ -25,4 +47,9 @@ pragmatistsApp.config(function($stateProvider, $urlRouterProvider) {
 pragmatistsApp.controller('UsersController', function($scope, users) {
     var vm = this;
     vm.users = users;
+});
+
+pragmatistsApp.controller('EditController', function($scope, $stateParams, users) {
+  var vm = this;
+  vm.user = users[$stateParams.index];
 });
